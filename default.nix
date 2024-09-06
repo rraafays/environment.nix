2e1458bc8d0562c7bd0da6d00aa8e96e35f85cdb
raf <rraf@tuta.io>
2024-09-06 16:19:28 +0100

    chore: removed many unused packages

--- default.nix
+++ default.nix
@@ -5,52 +5,29 @@
     bat
     brightnessctl
     btop
-    clang
-    csharp-ls
     detox
     direnv
     du-dust
     duf
-    elmPackages.elm-language-server
     fd
     gh
     git
-    jdt-language-server
     killall
-    lemminx
-    libqalculate
     libreoffice
     lsd
-    ltex-ls
-    lua-language-server
     lunarvim
-    nil
     nix-your-shell
-    nixpkgs-fmt
-    nodePackages.bash-language-server
-    nodePackages.prettier
-    nodePackages.sql-formatter
-    nodePackages.typescript-language-server
     p7zip
-    python311Packages.python-lsp-server
     python3
     rename
     ripgrep
-    rust-analyzer
-    rustfmt
-    scriptisto
     shfmt
-    sqls
     starship
     stylua
-    taplo
     tmux
     unzip
     uutils-coreutils-noprefix
-    vscode-langservers-extracted
     wget
-    xmlformat
-    xxd
     zoxide
   ];
 }
