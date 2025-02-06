51e2bdbac9c8d3911d61a4fc2202849e55e59693
Raf Shahid <raf@Rafs-MacBook-Pro.local>
2025-02-06 21:39:43 +0000

    update: made linux only packages optional

--- default.nix
+++ default.nix
@@ -1,40 +1,42 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
-  imports = [ ./restic.nix ];
-  environment.systemPackages = with pkgs; [
-    android-file-transfer
-    bat
-    brightnessctl
-    btop
-    detox
-    direnv
-    du-dust
-    duf
-    fd
-    ffmpeg
-    file
-    gcc
-    gh
-    killall
-    libreoffice
-    lsd
-    nix-your-shell
-    p7zip
-    python3
-    rename
-    ripgrep
-    shfmt
-    starship
-    stylua
-    tmux
-    unrar
-    unstable.neovim
-    unzip
-    usbutils
-    uutils-coreutils-noprefix
-    wget
-    woeusb-ng
-    zoxide
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      android-file-transfer
+      nixfmt
+      bat
+      btop
+      direnv
+      du-dust
+      duf
+      fd
+      ffmpeg
+      file
+      gcc
+      gh
+      killall
+      lsd
+      nix-your-shell
+      p7zip
+      python3
+      rename
+      ripgrep
+      shfmt
+      starship
+      stylua
+      tmux
+      unrar
+      unstable.neovim
+      unzip
+      uutils-coreutils-noprefix
+      wget
+      zoxide
+    ] ++ lib.optional stdenv.isLinux [
+      woeusb-ng
+      usbutils
+      libreoffice
+      detox
+      brightnessctl
+    ];
 }
