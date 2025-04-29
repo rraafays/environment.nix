c80440f86d41a164c1c9f03749935e5627b56e26
raf <rraf@tuta.io>
2025-04-29 23:02:50 +0100

    update: trash

--- toolset.nix
+++ toolset.nix
@@ -2,6 +2,91 @@
 
 {
   environment.systemPackages = with pkgs; [
+    (writeShellScriptBin "edit" ''
+      #!${stdenv.shell}
+      ${unstable.neovim}/bin/nvim "$@"
+    '')
+
+    (writeShellScriptBin "note" ''
+      #!${stdenv.shell}
+      ${unstable.neovim}/bin/nvim NOTE.md
+    '')
+
+    (writeShellScriptBin "trash" ''
+      #!${stdenv.shell}
+      if [ "$1" = "-l" ]; then
+        ${trash-cli}/bin/trash-list --trash-dir=$HOME/.Trash "''${@:2}"
+      elif [ "$1" = "-e" ]; then
+        if [ "$(uname)" = "Darwin" ]; then
+          osascript -e 'tell application "Finder" to empty trash'
+        else
+          ${trash-cli}/bin/trash-empty --trash-dir=$HOME/.Trash "''${@:2}"
+        fi
+      elif [ "$1" = "-r" ]; then
+        ${trash-cli}/bin/trash-restore --trash-dir=$HOME/.Trash "''${@:2}"
+      else
+        ${trash-cli}/bin/trash --trash-dir=$HOME/.Trash "$@"
+      fi
+    '')
+
+    (writeShellScriptBin "trash" ''
+      #!${stdenv.shell}
+      ${trash-cli}/bin/trash --trash-dir=$HOME/.Trash "$@"
+    '')
+
+    (writeShellScriptBin "system-info" ''
+      #!${stdenv.shell}
+      ${fastfetch}/bin/fastfetch "$@"
+    '')
+
+    (writeShellScriptBin "repo-info" ''
+      #!${stdenv.shell}
+      ${onefetch}/bin/onefetch -d churn commits lines-of-code --no-color-palette --true-color never
+      ${git}/bin/git status
+    '')
+
+    (writeShellScriptBin "todo" ''
+      #!${stdenv.shell}
+      if [ -z "$1" ]; then
+      	${fasole}/bin/fasole TODO.md
+      else
+      	${fasole}/bin/fasole "$@"
+      fi
+    '')
+
+    (writeShellScriptBin "work" ''
+      #!${stdenv.shell}
+      branch_name=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+      if [ $? -eq 128 ]; then
+      	echo "must be in a git repository to work!" >&2
+      	exit 1
+      fi
+      if [ -z "$1" ]; then
+      	${fasole}/bin/fasole "$branch_name.md"
+      else
+      	${fasole}/bin/fasole "$@"
+      fi
+    '')
+
+    (writeShellScriptBin "stamp" ''
+      #!${stdenv.shell}
+      if [ "$#" -lt 1 ]; then
+      	echo "You need to provide at least one file as an argument."
+      	exit 1
+      fi
+
+      current_time=$(date "+%Y%m%d%H%M%S")
+      echo "Current Time : $current_time"
+
+      for file_name in "$@"; do
+      	new_fileName=$current_time-$file_name
+      	echo "New FileName: " "$new_fileName"
+      	cp "$file_name" "$new_fileName"
+      done
+
+      echo "You should see new files generated with timestamp on them.."
+    '')
+
     (writeShellScriptBin "view" ''
       #!${stdenv.shell}
       if [ "$#" -ne 1 ]; then
@@ -57,69 +142,6 @@
       	echo "Filetype is unknown or unsupported."
       	;;
       esac
-    '')
-
-    (writeShellScriptBin "stamp" ''
-      #!${stdenv.shell}
-      if [ "$#" -lt 1 ]; then
-      	echo "You need to provide at least one file as an argument."
-      	exit 1
-      fi
-
-      current_time=$(date "+%Y%m%d%H%M%S")
-      echo "Current Time : $current_time"
-
-      for file_name in "$@"; do
-      	new_fileName=$current_time-$file_name
-      	echo "New FileName: " "$new_fileName"
-      	cp "$file_name" "$new_fileName"
-      done
-
-      echo "You should see new files generated with timestamp on them.."
-    '')
-
-    (writeShellScriptBin "todo" ''
-      #!${stdenv.shell}
-      if [ -z "$1" ]; then
-      	${fasole}/bin/fasole TODO.md
-      else
-      	${fasole}/bin/fasole "$@"
-      fi
-    '')
-
-    (writeShellScriptBin "work" ''
-      #!${stdenv.shell}
-      branch_name=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-      if [ $? -eq 128 ]; then
-      	echo "must be in a git repository to work!" >&2
-      	exit 1
-      fi
-      if [ -z "$1" ]; then
-      	${fasole}/bin/fasole "$branch_name.md"
-      else
-      	${fasole}/bin/fasole "$@"
-      fi
-    '')
-
-    (writeShellScriptBin "edit" ''
-      #!${stdenv.shell}
-      ${unstable.neovim}/bin/nvim "$@"
-    '')
-
-    (writeShellScriptBin "note" ''
-      #!${stdenv.shell}
-      ${unstable.neovim}/bin/nvim NOTE.md
-    '')
-
-    (writeShellScriptBin "system-info" ''
-      #!${stdenv.shell}
-      ${fastfetch}/bin/fastfetch "$@"
-    '')
-
-    (writeShellScriptBin "repo-info" ''
-      #!${stdenv.shell}
-      ${onefetch}/bin/onefetch -d churn commits lines-of-code --no-color-palette --true-color never
-      ${git}/bin/git status
     '')
   ];
 }
